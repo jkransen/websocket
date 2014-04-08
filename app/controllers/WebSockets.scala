2d0e6d6ea8ffa1ebc6bb807bdde8eacba210a71a
Jeroen Kransen <jeroen@kransen.nl>
2014-04-08 17:39:03 +0200

    simpele websocket toegevoegd

---
+++ app/controllers/WebSockets.scala
@@ -0,0 +1,28 @@
+package controllers
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.Random
+import play.api._
+import play.api.libs.iteratee.Enumerator
+import play.api.libs.iteratee.Iteratee
+import play.api.mvc._
+import scala.concurrent.duration._
+import play.api.libs.concurrent.Promise
+
+object WebSockets extends Controller {
+
+  def statusFeed = WebSocket.using[String] { implicit request =>
+    val in = Iteratee.ignore[String]
+    val out = Enumerator.repeatM {
+      Promise.timeout(getLoadAverage, 3 seconds)
+    }
+    (in, out)
+  }
+  
+  def aap = {
+  }
+  
+  def getLoadAverage: String = {
+    Random.nextInt(100) + "%"
+  }
+}
